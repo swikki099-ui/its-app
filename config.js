be3c86a86036913bd29aa4b706808573284dcd8f
swikki099-ui <swikki099@gmail.com>
2026-04-12 17:43:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Frontend Configuration (acts as a bridge for .env values in static HTML)
 const CONFIG = {
-    APP_VERSION: "v2.0.0",
+    APP_VERSION: "v2.0.5",
     APP_NAME: "ITS College ",
-    DOWNLOAD_URL: "https://www.dropbox.com/scl/fi/tf56j02br6tk5ecoj6hqz/stable_v2.0.0.apk?rlkey=w82xvkiu9nk46tknfl40e340d&st=ci55imz3&dl=1",
+    DOWNLOAD_URL: "https://www.dropbox.com/scl/fi/bd8mh9w5gy3j2otdpk507/new_v2.0.5.apk?rlkey=11o6oi87oxwag73mp3hrdw5zy&st=0dvtsux8&dl=1",
     DEV_NAME: "Neuro Dev Studio",
     SUPPORT_EMAIL: "shadowknight.gov@gmail.com",
     RATINGS_VALUE: "4.8",
